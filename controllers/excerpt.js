3dd153e1293e7f2f2c0a25e62602368bec689e1d
Hicaro Adriano <hicaro.adriano@gmail.com>
2017-08-11 16:43:42 -0400

    added default book in case of database error

--- controllers/excerpt.js
+++ controllers/excerpt.js
@@ -8,14 +8,15 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.get('/', function(req, res){
   Excerpt.get(function(err, excerpt) {
     if(err) {
+      var text = "Across the courtesy bay the white palaces of fashionable East Egg glittered along the water, and the history of the summer really begins on the evening I drove over there to have dinner with the Tom Bachanans.";
       return res.json({
-        letters: "",
-        length: 0,
-        title: "",
-        author: "",
-        url: "",
-        image: "",
-        promo: ""
+        letters: text.split(''),
+        length: text.length,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        url: "https://www.amazon.com/Great-Gatsby-F-Scott-Fitzgerald/dp/0743273567/ref=as_li_ss_il?_encoding=UTF8&psc=1&refRID=CXJPEVCD4ZM2ANRX4MQ5&linkCode=li3&tag=typingfromboo-20&linkId=114cc2ff0acc1cbce3205e0aae8fbb0a",
+        image: "https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=0743273567&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=typingfromboo-20",
+        promo: "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted \"gin was the national drink and sex the national obsession,\" it is an exquisitely crafted tale of America in the 1920s."
       });
     }
 
